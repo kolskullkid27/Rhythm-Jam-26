93fdb96c791ebfd671d4f182851b51bfcc4e105a
johngee22 <sinsoftheray@gmail.com>
2026-06-01 15:56:03 -0700

    odds and ends

--- Rhythm game/Scripts/Entities/note_trigger.gd
+++ Rhythm game/Scripts/Entities/note_trigger.gd
@@ -9,7 +9,7 @@ func _process(_delta: float) -> void:
 	# Shows if music note collides with this and triggers remove on the note
 	var note = area_2d.get_overlapping_areas()
 	
-	if Input.is_action_just_pressed(which_key):
+	if Input.is_anything_pressed():
 		if note.size() >= 1:
 			print(note[0])
 			note[0].get_parent().remove()
